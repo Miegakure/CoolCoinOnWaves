a77afc37180641ff2ff3f3c2955a4edb395802c9
Miegakure <miegakure@mail.ru>
2019-10-18 23:12:53 +0500

    add react app

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-/**
- * Very Cool Coin :D
- * @author Bagat Bagatov
- */
-
 const express = require('express');
 
 const app = express();
+
+app.use(express.static(__dirname+'/dist'));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root : __dirname});
