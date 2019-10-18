b2dca6fe38949945b8d324bdbb7a4b700205b83e
Miegakure <miegakure@mail.ru>
2019-10-18 22:48:15 +0500

    add project files

---
+++ server.js
@@ -0,0 +1,18 @@
+/**
+ * Very Cool Coin :D
+ * @author Bagat Bagatov
+ */
+
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root : __dirname});
+});
+
+let port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(__dirname);
+    console.log("Listening Port " + port);
+});
